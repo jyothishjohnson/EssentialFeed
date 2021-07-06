70c315a5694cd2dffb90a05f1fa16f8311dd1694
Jyothish Johnson <jyothish.johnson123@gmail.com>
2021-07-06 18:51:06 +0530

    created feed item model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jyothish Johnson on 06/07/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id : UUID
+    let imageURL : URL
+    let description : String?
+    let location : String?
+}
