3d25f68b153ccfc3c0152f39cf7a5cb76b1a6197
Jyothish Johnson <jyothish.johnson123@gmail.com>
2021-08-08 16:32:14 +0530

    Extract FeedStoreSpy to shared scope to remove duplication

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -28,45 +28,4 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         
         return (sut,store)
     }
-    
-    final class FeedStoreSpy: FeedStore {
-        
-        enum RecievedMessage: Equatable {
-            case deleteCacheMessage
-            case insertCacheMessage(items: [LocalFeedImage], timeStamp: Date)
-        }
-        
-        typealias DeletionCompletions = ((Error?) -> ())
-        typealias InsertionCompletions = ((Error?) -> ())
-        
-        private(set) var recievedMessages = [RecievedMessage]()
-        private var cacheDeletionFallbacks = [DeletionCompletions]()
-        private var cacheInsertionFallbacks = [InsertionCompletions]()
-        
-        func deleteCachedFeed(completion : @escaping DeletionCompletions){
-            recievedMessages.append(.deleteCacheMessage)
-            cacheDeletionFallbacks.append(completion)
-        }
-        
-        func insert(_ feed: [LocalFeedImage], withTimeStamp timeStamp: Date, completion: @escaping InsertionCompletions) {
-            cacheInsertionFallbacks.append(completion)
-            recievedMessages.append(.insertCacheMessage(items: feed, timeStamp: timeStamp))
-        }
-        
-        func completionDeletion(at index: Int = 0, with error: Error) {
-            cacheDeletionFallbacks[index](error)
-        }
-        
-        func completeInsertion(at index: Int = 0, with error: Error) {
-            cacheInsertionFallbacks[index](error)
-        }
-        
-        func completionDeletionSuccessfully(at index: Int = 0){
-            cacheDeletionFallbacks[index](nil)
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0){
-            cacheInsertionFallbacks[index](nil)
-        }
-    }
 }
