cc4e7763df19ac77a65d80ae267d699f5d3b70f1
Jyothish Johnson <jyothish.johnson123@gmail.com>
2021-08-28 14:01:13 +0530

    empty FeedStoreSpecs for CoreDate tests

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,60 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by Jyothish Johnson on 28/08/21.
+//
+
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+            
+    }
+    
+    func test_retrieve_deliversFoundCacheOnNonEmptyCache() {
+            
+    }
+    
+    func test_retrieve_hasNoSideEffects_OnNonEmptyCache() {
+            
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+            
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+            
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCachedValues() {
+            
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+            
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+            
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+            
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+            
+    }
+    
+    func test_store_sideEffectsRunSerially() {
+            
+    }
+    
+}
