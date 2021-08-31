60f27dea27e4e936ff56f1de868a2200d19ff9dc
Jyothish Johnson <jyothish.johnson123@gmail.com>
2021-08-31 21:01:47 +0530

    retrieve_hasNoSideEffects_OnNonEmptyCache

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -29,7 +29,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_retrieve_hasNoSideEffects_OnNonEmptyCache() {
-            
+        let sut = makeSUT()
+
+        assertThatRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
     }
     
     func test_insert_deliversNoErrorOnEmptyCache() {
