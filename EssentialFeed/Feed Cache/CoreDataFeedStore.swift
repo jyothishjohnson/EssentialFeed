6b1d85e1cbc298f03363946e27f27adb06884e13
Jyothish Johnson <jyothish.johnson123@gmail.com>
2021-08-31 20:57:08 +0530

    fetch logic into helper method

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -41,9 +41,7 @@ public final class CoreDataFeedStore: FeedStore {
         let context = self.context
         context.perform {
             do {
-                let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
-                request.returnsObjectsAsFaults = false
-                if let cache = try context.fetch(request).first {
+                if let cache = try ManagedCache.find(in: context) {
                     completion(.found(feed: cache.localFeed, timeStamp: cache.timestamp))
                 } else {
                     completion(.empty)
@@ -61,6 +59,12 @@ public final class CoreDataFeedStore: FeedStore {
         
         var localFeed: [LocalFeedImage] {
             return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+        }
+        
+        static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+            let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+            request.returnsObjectsAsFaults = false
+            return try context.fetch(request).first
         }
     }
 
