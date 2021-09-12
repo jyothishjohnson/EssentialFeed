003a0f9ea2508d7974f950bc5bf3aba4d74e615e
Jyothish Johnson <jyothish.johnson123@gmail.com>
2021-09-12 14:32:15 +0530

    add cache integration tests

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Jyothish Johnson on 12/09/21.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
