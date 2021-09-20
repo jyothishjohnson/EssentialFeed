e43a722d2fee8b1c924f6ec2a5c82d12ee5a10db
Jyothish Johnson <jyothish.johnson123@gmail.com>
2021-09-20 19:42:03 +0530

    test pullToRefresh_loadsFeed

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class FeedViewController: UIViewController {
+final class FeedViewController: UITableViewController {
     
     private var loader : FeedLoader?
     
@@ -20,6 +20,12 @@ class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+    
+    @objc func load(){
         loader?.load{ _ in }
     }
 }
@@ -39,6 +45,19 @@ class FeedViewControllerTests: XCTestCase {
         sut.loadViewIfNeeded()
         
         XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
+    func test_pullToRefresh_loadsFeed(){
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+        
+        sut.refreshControl?.allTargets.forEach({ target in
+            sut.refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach{
+                (target as NSObject).perform(Selector($0))
+            }
+        })
+        
+        XCTAssertEqual(loader.loadCallCount, 2)
     }
     
     //MARK: helpers
