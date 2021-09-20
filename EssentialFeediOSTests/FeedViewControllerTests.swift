083dab0c54b45c3a339f8c92189cef3411f4b625
Jyothish Johnson <jyothish.johnson123@gmail.com>
2021-09-20 13:01:46 +0530

    move sut creation to a helper function. makeSUT

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -27,15 +27,14 @@ class FeedViewController: UIViewController {
 class FeedViewControllerTests: XCTestCase {
 
     func test_init_doesNotLoadFeed(){
-        let loader = LoaderSpy()
-        _ = FeedViewController(loader: loader)
+        let (_,loader) = makeSUT()
+
         
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
     func test_viewDidLoad_loadsFeed(){
-        let loader = LoaderSpy()
-        let sut = FeedViewController(loader: loader)
+        let (sut,loader) = makeSUT()
         
         sut.loadViewIfNeeded()
         
@@ -43,6 +42,14 @@ class FeedViewControllerTests: XCTestCase {
     }
     
     //MARK: helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, loader)
+    }
     
     class LoaderSpy: FeedLoader {
         
